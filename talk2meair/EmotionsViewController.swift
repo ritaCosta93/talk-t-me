39558b274ce763bf0672308e7ce1415c001cd5ab
Rita <rita.dev4u@gmail.com>
2019-07-10 20:35:03 +0100

    Work in progress - Buttons creation

---
+++ talk2meair/EmotionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmotionsViewController.swift
+//  talk2meair
+//
+//  Created by ritacosta on 09/07/2019.
+//  Copyright © 2019 Rita Costa. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ talk2meair/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  talk2meair
+//
+//  Created by Rita Costa on 09/03/2019.
+//  Copyright © 2019 Rita Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
