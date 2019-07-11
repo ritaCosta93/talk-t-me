92afa642b238dafd13b99d19303623c24755a32e
Rita <rita.dev4u@gmail.com>
2019-07-11 18:05:47 +0100

    File structure update - Major bug fixing

--- talk2meair/FoodViewController.swift
+++ talk2meair/FoodViewController.swift
@@ -10,14 +10,7 @@ import UIKit
 
 class FoodViewController: UIViewController {
     
-    @IBOutlet var foodView: UIView!
-    @IBOutlet weak var homeBtn: UIButton!
-    @IBOutlet weak var peopleBtn: UIButton!
-    @IBOutlet weak var foodBtn: UIButton!
-    @IBOutlet weak var placesBtn: UIButton!
-    @IBOutlet weak var emotionsBtn: UIButton!
-    @IBOutlet weak var colorsBtn: UIButton!
-    @IBOutlet weak var numbersBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- talk2meair/PlacesViewController.swift
+++ talk2meair/PlacesViewController.swift
@@ -9,14 +9,7 @@
 import UIKit
 
 class PlacesViewController: UIViewController {
-    @IBOutlet var placesView: UIView!
-    @IBOutlet weak var homeBtn: UIButton!
-    @IBOutlet weak var peopleBtn: UIButton!
-    @IBOutlet weak var foodBtn: UIButton!
-    @IBOutlet weak var placesBtn: UIButton!
-    @IBOutlet weak var emotionsBtn: UIButton!
-    @IBOutlet weak var colorsBtn: UIButton!
-    @IBOutlet weak var numbersBtn: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
