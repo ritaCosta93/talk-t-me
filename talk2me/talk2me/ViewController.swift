88491856f0a4fc9af664a6769e5563a24fed2e4f
Rita <rita.dev4u@gmail.com>
2019-07-26 19:09:38 +0100

    UI Update

---
+++ talk2me/talk2me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  talk2me
+//
+//  Created by ritacosta on 22/07/2019.
+//  Copyright © 2019 ritacosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ talk2me/talk2meTests/talk2meTests.swift
@@ -0,0 +1,34 @@
+//
+//  talk2meTests.swift
+//  talk2meTests
+//
+//  Created by ritacosta on 22/07/2019.
+//  Copyright © 2019 ritacosta. All rights reserved.
+//
+
+import XCTest
+@testable import talk2me
+
+class talk2meTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
