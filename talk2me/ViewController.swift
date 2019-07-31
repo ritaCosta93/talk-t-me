8cf4d3572d95d2f67efd22549bb3c825fe01852d
Rita <rita.dev4u@gmail.com>
2019-07-31 22:38:08 +0100

    Button Actions added to main screen

--- talk2me/ViewController.swift
+++ talk2me/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,125 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func youBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func meBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func theyBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func heBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sheBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func itBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func breadBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func milkBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func waterBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func coffeeBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func chipsBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func cookiesBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func homeBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func bedroomBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func kitchenBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func bathroomBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func outsideBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func happyBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sadBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sickBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func hotBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func coldBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func blueBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pinkBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func purpleBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func cyanBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func yellowBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func orangeBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func redBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func blackBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func whiteBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func oneBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func twoBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func threeBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func fourBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func fiveBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sixBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sevenBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func eightBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nineBtnPressed(_ sender: Any) {
+    }
+    
 }
 
